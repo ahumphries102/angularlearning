2f286ea948187f131dcfda8640b251cf41df8cf9
Andrew Humphries <ahumphries102@gmail.com>
2020-10-31 21:48:12 -0500

    added some components, added some typescript functionality

---
+++ src/app/taco-alert/taco-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taco-alert',
+  templateUrl: './taco-alert.component.html',
+  styles: [
+  ]
+})
+export class TacoAlertComponent implements OnInit {
+  constructor() { 
+  }
+  tacoId = 40
+  getTacoId(){
+    return this.tacoId
+  }
+  ngOnInit(): void {
+  }
+
+}
